05369101b28d40ee99bb171c8303a2fa07bf1c9a
Istvan Nyolczas <nyolczas@gmail.com>
2017-10-26 14:13:39 +0200

    message

--- JavascriptAlapok/js/firstClass.js
+++ JavascriptAlapok/js/firstClass.js
@@ -7,5 +7,26 @@ console.log(osszead(1,2));
 function hello() {};
 // var hello = function() {}
 
+function addText(text1, text2, addFn) {
+    return addFn(text1, text2);
+}
 
+console.log(addText('alma', 'fa', osszead));
 
+console.log(addText('alma', 'fa', function(a, b) {
+    return a + b + '!' ;
+}));
+
+// valóságosabb példa: forEach
+
+var tomb = [1, 2, 3, 4];
+
+tomb.forEach(function(element, index, arr){
+    console.log(element, index, arr);
+});
+
+tomb.forEach(console.log);
+
+function shout(a) {console.log(a + '!'); };
+
+[5, 6, 7, 8].forEach(shout);
