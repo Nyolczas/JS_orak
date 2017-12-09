f24a224b6e30d9da89f58a30443472110d5f3766
Nyolczas <nyolczas@gmail.com>
2017-12-09 16:36:59 +0100

    circles

---
+++ ChrisCourse/js/random-circles.js
@@ -0,0 +1,20 @@
+var canvas = document.querySelector('canvas');
+
+var W = window.innerWidth;
+var H = window.innerHeight;
+
+canvas.width = W;
+canvas.height = H;
+
+var c = canvas.getContext('2d');
+
+c.strokeStyle = 'rgba(0, 125, 255, 0.5)';
+
+for (var i = 0; i < 5550; i++) {
+    c.beginPath();
+    var x = Math.random() * W;
+    var y = Math.random() * H;
+    var r = Math.random() * 200;
+    c.arc( x, y, r, 0, Math.PI * 2, false);
+    c.stroke();
+}
