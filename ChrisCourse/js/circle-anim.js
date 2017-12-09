e37affc2c38d6d6ea787aed202c14589b02f4cdf
Nyolczas <nyolczas@gmail.com>
2017-12-09 17:02:09 +0100

    anim

--- ChrisCourse/js/circle-anim.js
+++ ChrisCourse/js/circle-anim.js
@@ -11,14 +11,6 @@ var c = canvas.getContext('2d');
 // =============================================
 
 c.strokeStyle = 'rgba(0, 125, 255, 0.5)';
-
-for (var i = 0; i < 1550; i++) {
-    c.beginPath();
-    var x = Math.random() * W;
-    var y = Math.random() * H;
-    var min = 50;
-    var max = 150 - min;
-    var r = Math.random() * max + min;
-    c.arc( x, y, r, 0, Math.PI * 2, false);
-    c.stroke();
-}
+c.beginPath();
+c.arc( 200, 200, 30, 0, Math.PI * 2, false);
+c.stroke();
