9e307f3c1e1eeb1da755d508e1580fc33afdb7e1
Nyolczas <nyolczas@gmail.com>
2017-12-09 17:42:02 +0100

    circle anim

--- ChrisCourse/js/circle-anim.js
+++ ChrisCourse/js/circle-anim.js
@@ -9,8 +9,29 @@ canvas.height = H;
 var c = canvas.getContext('2d');
 
 // =============================================
-
 c.strokeStyle = 'rgba(0, 125, 255, 0.5)';
-c.beginPath();
-c.arc( 200, 200, 30, 0, Math.PI * 2, false);
-c.stroke();
+
+x = 50;
+var xSpeed = 2;
+y = 50;
+var ySpeed = 3; 
+radius = 20;
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, W, H);
+    c.beginPath();
+    c.arc(x, y, radius, 0, Math.PI * 2, false);
+    c.stroke();
+
+    if (x + radius > W || x - radius < 0) {
+        xSpeed = -xSpeed;
+    }
+    x += xSpeed;
+
+    if (y + radius > H || y - radius < 0) {
+        ySpeed = -ySpeed;
+    }
+    y += ySpeed;
+}
+
+animate();
