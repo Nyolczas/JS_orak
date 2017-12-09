cd28cceb45a47a3fa7b8ae97aba114534b3ef593
Nyolczas <nyolczas@gmail.com>
2017-12-09 16:42:00 +0100

    tuti circles

--- ChrisCourse/js/random-circles.js
+++ ChrisCourse/js/random-circles.js
@@ -10,11 +10,13 @@ var c = canvas.getContext('2d');
 
 c.strokeStyle = 'rgba(0, 125, 255, 0.5)';
 
-for (var i = 0; i < 5550; i++) {
+for (var i = 0; i < 1550; i++) {
     c.beginPath();
     var x = Math.random() * W;
     var y = Math.random() * H;
-    var r = Math.random() * 200;
+    var min = 50;
+    var max = 250 - min;
+    var r = Math.random() * max + min;
     c.arc( x, y, r, 0, Math.PI * 2, false);
     c.stroke();
 }
