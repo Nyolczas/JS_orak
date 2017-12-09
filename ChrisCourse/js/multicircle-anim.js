1e0f18db15cac688e5c2adf15c16911ad4de7ab5
Nyolczas <nyolczas@gmail.com>
2017-12-09 19:17:30 +0100

    anim circles

--- ChrisCourse/js/multicircle-anim.js
+++ ChrisCourse/js/multicircle-anim.js
@@ -11,9 +11,11 @@ var c = canvas.getContext('2d');
 c.strokeStyle = 'rgba(0, 125, 255, 0.33)';
 
 var minSpeed = 0.2;
-var speedFactor = 3;
-var minRadius = 50;
-var maxRadius = 150 - minRadius;
+var speedFactor = 0.5;
+var minRadius = 8;
+var maxRadius = 88 - minRadius;
+
+var quant = Math.floor((W + H) / 50);
 // =============================================
 
 function Circle(x, y, xSpeed, ySpeed, radius) {
@@ -39,32 +41,32 @@ function Circle(x, y, xSpeed, ySpeed, radius) {
             this.ySpeed = -this.ySpeed;
         }
         this.y += this.ySpeed;
-        
+
         this.draw();
     }
 }
 
-var circle = new Circle(200, 200, 3, 3, 20);
-// 
+var CircleArray = [];
 
+for (var i = 0; i < quant; i++) {
+    var x = Math.random() * W;
+    var y = Math.random() * H;
+    var xSpeed = (Math.random() - 0.5) * speedFactor;
+    xSpeed += xSpeed * minSpeed;
+    var ySpeed = (Math.random() - 0.5) * speedFactor;
+    ySpeed += ySpeed * minSpeed;
+    radius = Math.random() * maxRadius + minRadius;
 
-
-x = Math.random() * W;
-var xSpeed = (Math.random() - 0.5) * speedFactor;
-xSpeed += xSpeed * minSpeed;
-
-y = Math.random() * H;
-var ySpeed = (Math.random() - 0.5) * speedFactor;
-ySpeed += ySpeed * minSpeed;
-
-radius = Math.random() * maxRadius + minRadius;;
+    CircleArray.push(new Circle(x, y, xSpeed, ySpeed, radius));
+}
 
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, W, H);
 
-    circle.update();
-
+    for (var i = 0; i < CircleArray.length; i++) {
+        CircleArray[i].update();
+    }
 }
 
 animate();
